c0823e62e092d7272773333e57af2fcaf0f750b9
Ivan Pozdov <Ivan.pozdov58@gmail.com>
2023-07-10 18:46:04 +0300

    CrashTest

---
+++ src/test/java/ru/netology/PostmanEchoTest.java
@@ -0,0 +1,23 @@
+package ru.netology;
+
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
+
+class PostmanEchoTest {
+
+    @Test
+    void shouldReturnSendData(){
+
+        given()
+            .baseUri("https://postman-echo.com")
+            .body("HI")
+            .when()
+            .post("/post")
+            .then()
+            .statusCode(200)
+            .body("data", equalTo("DATA"));
+
+        }
+}
